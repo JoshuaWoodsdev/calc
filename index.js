b2cf2403d173268f01e11a36b7bb6448b56552e4
Joshua Woods <nordev@protonmail.com>
2022-04-27 13:45:29 -0500

    update

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
+
+//- the program needs to prompt the user for an operation. How will that be stored?
+//- how will you decide if the users input is a valid operation? What should the users input be checked against?
+//- after you validate the operation, the program should prompt the user for the first and second number. How will you store those values?
+//- at this point you have the operation and both numbers. Now you can implement the math calculation on the numbers based on the input
+//Logic: Conduct a check on each input
+
 var readlineSync = require('readline-sync');
-
 var maths = [
-    '+',
-     '-',
-     '*',
-     '/'
-    ];
-
-var problem = [ ];   
-
-var userChoice = readlineSync.question('What math operation would you like to perform? ');
-
-if ( userChoice == maths) {
- //if it checks out then it is pushed into the problem array
-} else {
-    //do a repeat of userChoice
-}
-
-//readline prompt
-var firstNumber = readlineSync.questionInt();//collect the first number and push into the problem array
- push
-
-console.log();
+              *,
+              +,
+              -,
+              /
+];
+//Start
+var userMath = readlineSync.promt('What maths would you like to perform? ');
+console.log(' hi you choose' + userMath + '!');
