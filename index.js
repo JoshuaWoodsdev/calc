19f77a6ee462287cd9b5262a3727f6cf04edf8e4
Joshua Woods <nordev@protonmail.com>
2022-05-02 23:05:57 -0500

    push update

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 
 //- how will you decide if the users input is a valid operation? What should the users input be checked against?
 //- at this point you have the operation and both numbers. Now you can implement the math calculation on the numbers based on the input
-//Logic: Conduct a check on each input
-
 var readlineSync = require('readline-sync');
 var maths = ['+', '*', '/', '-'];
 
-var userMath = readlineSync.question('What maths would you like to perform? ');
-
+var userMath = readlineSync.question(' What maths would you like to perform? ');
+   //setup up the array check
+   if true{
+     console.log(yesss);
+  } else {
+     console.log(nooooo);
+   }
+   console.log(maths.includes(userMath));
 
 var userMath2 = readlineSync.questionInt('What is the first number?');
-
 var userMath3 = readlineSync.questionInt('What is the second numbers? ');
 
-
-
-
-function math(n,m) {
-      return n m
-}
-
-console.log('call function should go here');
+//don the math...
