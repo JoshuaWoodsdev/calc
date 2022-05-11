8f11e717494a95fc36c797fb1ce315a8286e5bac
Joshua Woods <nordev@protonmail.com>
2022-05-11 12:09:05 -0500

    setup

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-//setup of program calling the packet
 var readlineSync = require('readline-sync'),
 math = ['+', '-', '/', '*'],
 index = readlineSync.keyInSelect(math, 'What math do you want to perform?');
 console.log('You choose ' + math[index]);
 //the IF check
-//solved the error I was having here by asking IF is was wrong instead of right
+//solved the error
 if(!math.includes(index)) {
   console.log('ok it is in this and working ');
   } else {
-  console.log('The (math.includes(index) is returning false? No matter how I set it up? Grammer error?  ');
+  console.log('how to get this to shut off and restart? IDK maybe break?');
 }
-//how do i force them to restart?
-
-
+//how do I force them to restart?
 var firstNumber = readlineSync.questionInt(' Please enter the first number ' );
 var secondNumber = readlineSync.questionInt(' Please enter the second number ');
 
@@ -30,9 +27,6 @@ function myFunction() {
     console.log( firstNumber / secondNumber );
   }
 }
-//seems like I have a error in my function
-//function myFunction() {
-  //console.log("Hello World!");
-//}
+//I don't know why I needed the [] around index?
 
 myFunction();
